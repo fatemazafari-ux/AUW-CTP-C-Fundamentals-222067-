8eb8a499cc7852e0cf96f80c431919b17bcd4a11
fatemazafari-ux <fatema.zafari@auw.edu.bd>
2026-02-17 23:40:47 +0600

    Add files via upload

---
+++ q44-222067.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a[5],count=0;
+    for(int i=0;i<5;i++){
+        scanf("%d",&a[i]);
+        if(a[i]>0) count++;
+    }
+    printf("Positive Numbers Count = %d",count);
+}
